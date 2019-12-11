ee492cb86ef843578ec63e35048072c3c1eb7ae5
Jesse Byers <jesse.s.byers@gmail.com>
2019-12-11 17:13:01 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -50,6 +50,7 @@ end
 
   def turn
     puts "Please enter 1-9:"
+    binding.pry
     if board.valid_move?(current_player.move(board))
       board.update(current_player.move(board), current_player)
     end
